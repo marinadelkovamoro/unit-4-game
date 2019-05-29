47d67739331a25ef623581e0a619091c656e6429
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-05-29 12:41:21 -0700

    fixing bugs in conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,62 @@
 var score = 0;
 var randomNum = randomNumGenerator(1, 100);
 $("#randomNumber").text(randomNum)
-
-// var randomNumber = Math.floor((Math.random() * 100) + 1);
-// // var x = Math.floor((Math.random() * 100) + 1);
-// //document.getElementById("randomNumber").innerHTML = x;
-// $("#randomNumber").text(randomNumber);
+var losses = 0;
+var wins = 0;
+var btnVal = 0;
 
 // set up function for random numbers generation  
 function randomNumGenerator(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
 
-// set a random number for each gem
-for (var i = 0; i < $(".gems").length; i++) {
-    var gemNum = randomNumGenerator(1, 12);
-    $("button-" + i).val(gemNum);
-}
 
-// reset the game and assign a new random number between 1 and 100
-function resetGame() {
-    randomNum = Math.floor(Math.random() * 100) + 1;
-    // display on the page
-    $("#randomNumber").text(randomNum);
-    // reset the score to 0
-    score = 0;
-    // display the new score on the page
-    $("#score").text(score);
 
-// setting the gems with random numbers
-    for (var i = 0; i < $(".gems").length; i++) {
-        var gemNum = randomNumGenerator(1, 12);
-        $("button-" + i).val(gemNum);
-    }
-}
 
 // when the user clicks on the gems 
 $(".gems").on("click", function () {
-    $("#score").text("");
-    var btnVal = parseInt($(this).val());
-    // console.log(btnVal);
-    score = score +btnVal;
-    $("#wins-num").text(score);
-    $("#losses-num").text(score);
-
+    // btnVal.parseInt($(this).val()); NEED TO FIX THIS
+    console.log(btnVal);
+    score = score + btnVal;
+    $("#score").text();
 
     // check if the score matches the random number
     if (score === randomNum) {
         // alert("You won!")
-        $("#score").text("🏆 You won! 🏆")
+        wins++;
+        $("#score").text("🏆 You won! 🏆");
+        $("#wins-num").text(wins);
         resetGame();
 
     } else if (score > randomNum) {
-        $("#score").text("😿 You lost! 😿")
+        // alert("You lost!")
+        losses++;
+        $("#score").text("😿 You lost! 😿");
+        $("#losses-num").text(losses);
         resetGame();
     }
 });
+    // reset the game and assign a new random number between 1 and 100
+    function resetGame() {
+        
+        randomNum = Math.floor(Math.random() * 100) + 1;
+        // display on the page
+        $("#randomNumber").text(randomNum);
+        // reset the score to 0
+        score = 0;
+        // display the new score on the page
+        $("#score").text(score);
+
+        // setting the gems with random numbers
+        for (var i = 0; i < $(".gems").length; i++) {
+            var gemNum = randomNumGenerator(1, 12);
+            $("button-" + i).val(gemNum);
+        }
+    }
+
+
+// call resetGame function when the page reload
+resetGame();
 
 
 
